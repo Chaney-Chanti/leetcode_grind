31deb5c1562b197a62362a0a50586591695bab01
Chaney Chantipaporn <66577982+Chaney-Chanti@users.noreply.github.com>
2026-03-04 14:38:16 -0800

    Implement recursive max depth function for binary tree
    Added a recursive solution for calculating the maximum depth of a binary tree.

--- max_depth_of_binary_tree.py
+++ max_depth_of_binary_tree.py
@@ -28,4 +28,20 @@ class Solution:
             depth += 1   # finished one full level
 
         return depth
-        
+
+################## Recursive Solution #####################
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def init(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+    class Solution(object):
+        def maxDepth(self, root):
+            def dfs(node):
+                if not node:
+                    return 0
+                left_depth = dfs(node.left)
+                right_depth = dfs(node.right)
+                return max(left_depth, right_depth) + 1
